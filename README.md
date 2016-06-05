f775789dd47aca73b8d3bcea24e5d9901a26c996
akhoury <bentael@gmail.com>
2016-06-05 16:12:43 -0400

    kazam

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# nodebb-require
+# nodebb-plugin-require
 Easily require NodeBB/src modules from within any plugin when running that as a stand-alone script
+
+## Usage
+
+```
+// from within other plugins
+
+var nodebbRequire = require('nodebb-plugin-require');\
+
+// to require the /path/to/NodeBB/src/user
+var File = nodebbRequire('/src/file');
+
+// you can also print out the full filepath
+console.log(nodebbRequire.fullpath);
+
+```
+
+## other functions
+You can these 2 for ther other packages as well, not just `nodebb`
+
+### `nodebbRequire.isPackageDirectory(name, dir)`
+
+```
+
+nodebbRequire.isPackageDirectory('lodash', '/path/to/somewhere'); // true/false
+
+```
+
+### `nodebbRequire.findPackageDirectory(name, startDir)`
+
+```
+nodebbRequire.findPackageDirectory('lodash', 'path/to/somwhere/dark/and/scary');
+
+```
+
+## nodebb helper functions
+
+### `nodebbRequire.isNodebbDirectory(dir)`
+### `nodebbRequire.findNodebbDirectory(startDir)`
---
+++ index.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var fs = require('fs');
+
+var isPackageDirectory = function (name, dir) {
+	var pkg;
+	try {
+		pkg = JSON.parse(fs.readFileSync(path.join(dir, './package.json'), 'utf8'));
+		return pkg.name == name;
+	} catch (e) {
+		return false;
+	}
+};
+
+var findPackageDirectory = function (name, dir) {
+	while (dir) {
+		if (isPackageDirectory(name, dir)) {
+			return dir;
+		}
+		var parts = dir.split(path.sep);
+		parts.pop();
+		dir = parts.join(path.sep);
+	}
+	throw new Error("Cannot find " + name + " installation path, are you sure you installed this module somewhere under /path/to/" + name + "/node_modules?");
+};
+
+
+var isNodebbDirectory = function (dir) {
+	return isPackageDirectory("nodebb", dir);
+}
+
+var findNodebbDirectory = function (startDir) {
+	return findPackageDirectory("nodebb", startDir);
+};
+
+var fullpath = findNodebbDirectory(__dirname);
+
+var nodebbRequire = function (relative) {
+	return require(path.join(fullpath, relative));
+};
+
+nodebbRequire.fullpath = fullpath;
+nodebbRequire.isPackageDirectory = isNodebbDirectory;
+nodebbRequire.findPackageDirectory = isNodebbDirectory;
+
+// helpers?
+nodebbRequire.isNodebbDirectory = isNodebbDirectory;
+nodebbRequire.findNodebbDirectory = findNodebbDirectory;
+
+module.exports = nodebbRequire;
